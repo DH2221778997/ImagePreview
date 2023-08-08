839dfed590cc0d3ee8078d04bfe1b8e3ffb0e8fa
daihai <2221778997@qq.com>
2023-08-08 08:17:17 +0800

    zz删掉一个z

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useRef, ChangeEventHandler } from "react";
 import ImagePreview from "./ImagePreview";
 import fileToString from "./utils";
 function App() {
-  const t = "zz";
+  const t = "z";
 
   const [img, setImg] = useState<string>("");
   const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
